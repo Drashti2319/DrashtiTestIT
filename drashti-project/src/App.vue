a2ca2cb8da00a9d0cbe38f3cd93580287abf3ddf
Drashti Parikh <parikhdrashti19@gmail.com>
2024-05-04 11:02:43 +0530

    Question B implemented

--- drashti-project/src/App.vue
+++ drashti-project/src/App.vue
@@ -1,17 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1>Drashti - Innovalabs Technologies Test</h1>
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/Users">User List (using store)</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
---
+++ drashti-project/src/components/SearchFilter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="text" v-model="searchString" placeholder="Search..." @input="search" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchString: ''
+    }
+  },
+  methods: {
+    search() {
+      this.$emit('search', this.searchString)
+    }
+  }
+}
+</script>
